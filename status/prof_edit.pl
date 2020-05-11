28ea5adb93a712af3770afabcea3e6b325647e88
catdingding <tigerrayson@gmail.com>
2020-05-12 00:46:48 +0800

    refactor prof

--- status/prof_edit.pl
+++ status/prof_edit.pl
@@ -1,70 +1,61 @@
-#_/_/_/_/_/_/_/_/_/#
-#_/    プロフィール    _/#
-#_/_/_/_/_/_/_/_/_/#
-
 sub prof_edit {
-
 	&chara_open;
-	&con_open;
     	
 	open(IN,"./logfile/prof/$mid.cgi");
 	@PROF_DATA = <IN>;
 	close(IN);
 
-	if(@PROF_DATA == ()){
-		$com1="";
-		$com2="";
-	}
-	else{
-		$com2="@PROF_DATA";
-	}
 	&header;
 	print <<"EOM";
-<TABLE CLASS=TC WIDTH="100%" height=100%>
-<TBODY><TR>
-<TD BGCOLOR=$FCOLOR WIDTH=100% height=5 align=center><font color=$FCOLOR2 size=4>＜＜<B> * 更新自傳 *</B>＞＞</font></TD>
-</TR><TR>
-<TD height="5">
-<TABLE border="0" width=100%><TBODY>
-<TR><TD><img src=\"$IMG/etc/machi.jpg\"></TD><TD width="100%" bgcolor=000000><font color=ffffff>在此可以隨時更新自傳。<BR>可以輸入你要大家點閱你時可以看到的內容，請不要輸入不雅的字眼。</font></TD>
-</TR>
-</TBODY></TABLE>
-</TD>
-</TR><TR>
-<TD WIDTH=100% align=center bgcolor=$FCOLOR2>
-$BACKTOWNBUTTON
-</TD>
-</TR>
-<TR>
-<td colspan=2 bgcolor=$FCOLOR2 align=center>
-<table CLASS=TC width=80%>
-<tr>
-<TD align=center><font color=$FCOLOR2>$mname的自傳</font></TD>
-</tr>
-<tr>
-<TD bgcolor=000000><font color=$FCOLOR2>$com2</font></TD>
-</tr>
-</table>
-</td>
-</TR>
-<TR>
-<TD align=center bgcolor=$FCOLOR2>
-
-<br><form action="./status.cgi" method="post">
-請輸入自傳內容：<BR>
-<textarea name=message cols=50 rows=10>$com1
-</TEXTAREA> <img src="$IMG/chara/$mchara.gif"><p>
-<input type=hidden name=id value=$mid>
-<INPUT type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-<input type=hidden name=mode value=prof_write>
-<input type=submit CLASS=MFC value="更新自傳">
-</form>
-</font>
-
-</CENTER>
-</TD>
-</TR>
-</TBODY></TABLE>
+	<table class=TC width="100%" height=100%>
+		<tbody>
+			<tr>
+				<td bgcolor=$FCOLOR width=100% height=5 align=center><font color=$FCOLOR2 size=4>＜＜<B> * 更新自傳 *</B>＞＞</font></td>
+			</tr>
+			<tr>
+				<td height="5">
+					<table border="0" width=100%>
+						<tr><td>
+							<img src=\"$IMG/etc/machi.jpg\"></td><td width="100%" bgcolor=000000>
+							<font color=ffffff>在此可以隨時更新自傳。<BR>可以輸入你要大家點閱你時可以看到的內容，請不要輸入不雅的字眼。</font>
+						</td></tr>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td width=100% align=center bgcolor=$FCOLOR2>
+					$BACKTOWNBUTTON
+				</td>
+			</tr>
+			<tr>
+				<td colspan=2 bgcolor=$FCOLOR2 align=center>
+					<table class=TC width=80%>
+						<tr>
+							<td align=center><font color=$FCOLOR2>$mname的自傳</font></td>
+						</tr>
+						<tr>
+							<td bgcolor=000000><font color=$FCOLOR2>@PROF_DATA</font></td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td align=center bgcolor=$FCOLOR2>
+					<br>
+					<form action="./status.cgi" method="post">
+						請輸入自傳內容：<BR>
+						<textarea name=message cols=50 rows=10></textarea>
+						<img src="$IMG/chara/$mchara.gif">
+						<input type=hidden name=id value=$mid>
+						<input type=hidden name=pass value=$mpass>
+						<input type=hidden name=rmode value=$in{'rmode'}>
+						<input type=hidden name=mode value=prof_write>
+						<input type=submit class=MFC value="更新自傳">
+					</form>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 EOM
 
 	&footer;
