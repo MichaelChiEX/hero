b7e23fd772d4ef7fffa474fc43b41612dce408f1
catdingding <tigerrayson@gmail.com>
2020-05-12 16:22:24 +0800

    refactor sk_set

--- status/sk_set2.pl
+++ status/sk_set2.pl
@@ -11,31 +11,31 @@ sub sk_set2{
 		$msk="$msk[0],$in{'skill2'}";
 	}
 	&chara_input;
-
 	
 	&header;
 	print <<"EOF";
-<TABLE border="0" width="80%" bgcolor="#ffffff" height="150" align=center CLASS=FC>
-  <TBODY>
-    <TR>
-      <TD colspan="2" align="center" bgcolor="#993300"><FONT color="#ffffcc">奧義變更</FONT></TD>
-    </TR>
-    <TR>
-      <TD bgcolor="#ffffcc" width=20% align=center><img src="$IMG/etc/palace2.jpg"></TD>
-      <TD bgcolor="#330000"><FONT color="#ffffcc">已成功變更了奧義。</FONT></TD>
-    </TR>
-    <TR>
-      <TD colspan="2" align="right">
-	<form action="./status.cgi" method="POST">
-	<INPUT type=hidden name=id value=$mid>
-	<INPUT type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-	<INPUT type=hidden name=mode value=sk_set>
-	<INPUT type=submit CLASS=FC value=回奧義變更畫面></form>
-$BACKTOWNBUTTON
-    </TD>
-    </TR>
-  </TBODY>
-</TABLE>
+	<table border="0" width="80%" bgcolor="#ffffff" height="150" align=center CLASS=FC>
+		<tbody>
+			<tr>
+				<td colspan="2" align="center" bgcolor="#993300"><font color="#ffffcc">奧義變更</font></td>
+			</tr>
+			<tr>
+				<td bgcolor="#ffffcc" width=20% align=center><img src="$IMG/etc/palace2.jpg"></td>
+				<td bgcolor="#330000"><font color="#ffffcc">已成功變更了奧義。</font></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="right">
+				<form action="./status.cgi" method="POST">
+				<input type=hidden name=id value=$mid>
+				<input type=hidden name=pass value=$mpass>
+				<input type=hidden name=rmode value=$in{'rmode'}>
+				<input type=hidden name=mode value=sk_set>
+				<input type=submit CLASS=FC value=回奧義變更畫面></form>
+					$BACKTOWNBUTTON
+				</td>
+			</tr>
+		</tbody>
+	</table>
 EOF
 	&footer;
 	exit;
