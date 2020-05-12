59249dafd02e7980931f94e986ce96984f11b80c
catdingding <tigerrayson@gmail.com>
2020-05-12 23:04:20 +0800

    refactor equip

--- status/equip3.pl
+++ status/equip3.pl
@@ -17,64 +17,63 @@ sub equip3{
         $ext_ab_item[$in{'abno'}]-=$in{'num'};
         if($ext_ab_item[$in{'abno'}]<0){&error("你的$it_name[$in{'abno'}]數量不足");}
         $it_dmg=$in{'abno'};
-                        $upval = $in{'num'};
-                        if($it_dmg eq "0" && $mmaxstr<500){
-                                $mmaxstr += $upval;
-                                if($mmaxstr>500){&error("吃下後力量會超過上限！");}
-                                $mess.="<font color=orange>力量 界限值上昇了$upval點！！</font><BR>";
-                        }elsif($it_dmg eq "1" && $mmaxvit<500){
-                                $mmaxvit += $upval;
-                                if($mmaxvit >500){&error("吃下後生命會超過上限！");}
-                                $mess.="<font color=orange>生命 界限值上昇了$upval點！！</font><BR>";
-                        }elsif($it_dmg eq "2" && $mmaxint<500){
-                                $mmaxint += $upval;
-                                if($mmaxint >500){&error("吃下後智力會超過上限！");}
-                                $mess.="<font color=orange>智力 界限值上昇了$upval點！！</font><BR>";
-                        }elsif($it_dmg eq "3" && $mmaxmen<500){
-                                $mmaxmen += $upval;
-                                if($mmaxmen >500){&error("吃下後精神會超過上限！");}
-                                $mess.="<font color=orange>精神 界限值上昇了$upval點！！</font><BR>";
-                        }elsif($it_dmg eq "4" && $mmaxdex<500){
-                                $mmaxdex += $upval;
-                                if($mmaxdex >500){&error("吃下後運氣會超過上限！");}
-                                $mess.="<font color=orange>運氣 界限值上昇了$upval點！！</font><BR>";
-                        }elsif($it_dmg eq "5" && $mmaxagi<500){
-                                $mmaxagi += $upval;
-                                if($mmaxagi >500){&error("吃下後速度會超過上限！");}
-                                $mess.="<font color=orange>速度 界限值上昇了$upval點！！</font><BR>";
-                        }else{
-                                &error("<font color=red>再吃$in{'num'}個你的$it_name[$it_dmg]就超過500上限了</font>");
-                        }
-                        $mmax="$mmaxstr,$mmaxvit,$mmaxint,$mmaxmen,$mmaxdex,$mmaxagi,$mmaxlv";
+        $upval = $in{'num'};
+        if($it_dmg eq "0" && $mmaxstr<500){
+            $mmaxstr += $upval;
+            if($mmaxstr>500){&error("吃下後力量會超過上限！");}
+            $mess.="<font color=orange>力量 界限值上昇了$upval點！！</font><BR>";
+        }elsif($it_dmg eq "1" && $mmaxvit<500){
+            $mmaxvit += $upval;
+            if($mmaxvit >500){&error("吃下後生命會超過上限！");}
+            $mess.="<font color=orange>生命 界限值上昇了$upval點！！</font><BR>";
+        }elsif($it_dmg eq "2" && $mmaxint<500){
+            $mmaxint += $upval;
+            if($mmaxint >500){&error("吃下後智力會超過上限！");}
+            $mess.="<font color=orange>智力 界限值上昇了$upval點！！</font><BR>";
+        }elsif($it_dmg eq "3" && $mmaxmen<500){
+            $mmaxmen += $upval;
+            if($mmaxmen >500){&error("吃下後精神會超過上限！");}
+            $mess.="<font color=orange>精神 界限值上昇了$upval點！！</font><BR>";
+        }elsif($it_dmg eq "4" && $mmaxdex<500){
+            $mmaxdex += $upval;
+            if($mmaxdex >500){&error("吃下後運氣會超過上限！");}
+            $mess.="<font color=orange>運氣 界限值上昇了$upval點！！</font><BR>";
+        }elsif($it_dmg eq "5" && $mmaxagi<500){
+            $mmaxagi += $upval;
+            if($mmaxagi >500){&error("吃下後速度會超過上限！");}
+            $mess.="<font color=orange>速度 界限值上昇了$upval點！！</font><BR>";
+        }else{
+            &error("<font color=red>再吃$in{'num'}個你的$it_name[$it_dmg]就超過500上限了</font>");
+        }
+        $mmax="$mmaxstr,$mmaxvit,$mmaxint,$mmaxmen,$mmaxdex,$mmaxagi,$mmaxlv";
 
         &ext_input;
         &chara_input;
         &header;
 	
 	print <<"EOF";
-<TABLE border="0" width="80%" align=center height="150" CLASS=FC>
-  <TBODY>
-    <TR>
-      <TD colspan="2" align="center" bgcolor="$FCOLOR"><FONT color="$FCOLOR2">使用</FONT></TD>
-    </TR>
-    <TR>
-      <TD bgcolor="$FCOLOR2" width=20% align=center><img src="$IMG/etc/inn.jpg"></TD>
-      <TD bgcolor="#330000"><FONT color="$FCOLOR2">你使用了$in{'num'}個<font color=blue>$it_name[$it_dmg]$mess</font>。</FONT></TD>
-    </TR>
-    <TR>
-    <TD colspan="2" align="center" bgcolor="ffffff">
-<form action="./status.cgi" method=post id="statusf" target="actionframe">
-      <input type=hidden name=id value=$mid>
-      <INPUT type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-      <input type=hidden name=mode value=equip>
-        <input type=submit value="回到使用畫面">
-</form>
-$BACKTOWNBUTTON
-    </TD>
-    </TR>
-  </TBODY>
-</TABLE>
-<center></center>
+    <table border="0" width="80%" align=center height="150" class=FC>
+        <tbody>
+            <tr>
+                <td colspan="2" align="center" bgcolor="$FCOLOR"><font color="$FCOLOR2">使用</font></td>
+            </tr>
+            <tr>
+                <td bgcolor="$FCOLOR2" width=20% align=center><img src="$IMG/etc/inn.jpg"></td>
+                <td bgcolor="#330000"><font color="$FCOLOR2">你使用了$in{'num'}個<font color=blue>$it_name[$it_dmg]$mess</font>。</font></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center" bgcolor="ffffff">
+                    <form action="./status.cgi" method=post id="statusf" target="actionframe">
+                    <input type=hidden name=id value=$mid>
+                    <INPUT type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
+                    <input type=hidden name=mode value=equip>
+                    <input type=submit value="回到使用畫面">
+                    </form>
+                    $BACKTOWNBUTTON
+                </td>
+            </tr>
+        </tbody>
+    </table>
 EOF
 &footer;
 exit;
