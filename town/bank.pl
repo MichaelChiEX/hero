4bbaed937abbf5837b9ffedc816b0374fa4464f4
catdingding <tigerrayson@gmail.com>
2020-05-14 10:51:07 +0800

    refactor bank

--- town/bank.pl
+++ town/bank.pl
@@ -2,57 +2,58 @@ sub bank {
 	&chara_open;
 	&header;
 	&status_print;
-	$inn_gold=int(($mmaxhp-20)/3+$mmaxmp/3+($mstr+$mvit+$mint+$mdex+$mfai+$magi)/5);
-	if($inn_gold<5){$inn_gold=5;}
-	#if($mgold<$inn_gold && $mbank<$inn_gold){$mes="お金をお持ちで無いようですね。今回は特別サービスにしますよ。";}
 	print <<"EOF";
-<table border="0" width="80%" align=center bgcolor="#ffffff" height="150" CLASS=FC>
-  <tbody>
-    <tr>
-      <td colspan="2" align="center" bgcolor="#993300"><font color="#ffffcc">銀行</font></td>
-    </tr>
-    <tr>
-      <td bgcolor="#ffffcc" width=20% align=center><img src="$IMG/etc/pub.jpg"></td>
-      <td bgcolor="#330000"><font color="#ffffcc">歡迎光臨 $mname 你目前在銀行共有$mbank Gold。<br>請選擇服務項目</font></td>
-    </tr>
-    <tr>
-      <td colspan="2" align="center">
-	$STPR
-	<br>
-	<銀行中的資金：$mbank Gold>
-	<form action="./town.cgi" method="post">
-	<input type=text size=5 name=azuke>萬Gold
-	<input type=hidden name=id value=$mid>
-	<input type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-	<input type=hidden name=mode value=banka>
-	
-	<input type=submit value=存入 CLASS=MFC></form>
-	<form action="./town.cgi" method="post">
-	<input type=hidden name=azuke value=$mgold>
-	<input type=hidden name=id value=$mid>
-	<input type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-	
-	<input type=hidden name=mode value=bankall>
-	<input type=submit value=全部存入 CLASS=MFC></form>
-	<form action="./town.cgi" method="post">
-	<input type=text size=5 name=hiki>萬Gold
-	<input type=hidden name=id value=$mid>
-	<input type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-	
-	<input type=hidden name=mode value=bankh>
-	<input type=submit value=取出 CLASS=MFC></form>
-	<form action="./town.cgi" method="post">
-	<input type=hidden name=azuke value=$mbank>
-	<input type=hidden name=id value=$mid>
-	<input type=hidden name=pass value=$mpass><input type=hidden name=rmode value=$in{'rmode'}>
-	
-	<input type=hidden name=mode value=bankhall>
-	<input type=submit value=全部取出 CLASS=MFC></form>
-$BACKTOWNBUTTON
-	</td>
-    </tr>
-  </tbody>
-</table>
+	<table border="0" width="80%" align=center bgcolor="#ffffff" height="150" CLASS=FC>
+		<tbody>
+			<tr>
+				<td colspan="2" align="center" bgcolor="#993300"><font color="#ffffcc">銀行</font></td>
+			</tr>
+			<tr>
+				<td bgcolor="#ffffcc" width=20% align=center><img src="$IMG/etc/pub.jpg"></td>
+				<td bgcolor="#330000"><font color="#ffffcc">歡迎光臨 $mname 你目前在銀行共有$mbank Gold。<br>請選擇服務項目</font></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="center">
+				$STPR
+				<br>
+				<銀行中的資金：$mbank Gold>
+				<form action="./town.cgi" method="post">
+					<input type=text size=5 name=azuke>萬Gold
+					<input type=hidden name=id value=$mid>
+					<input type=hidden name=pass value=$mpass>
+					<input type=hidden name=rmode value=$in{'rmode'}>
+					<input type=hidden name=mode value=banka>
+					<input type=submit value=存入 CLASS=MFC>
+				</form>
+				<form action="./town.cgi" method="post">
+					<input type=hidden name=azuke value=$mgold>
+					<input type=hidden name=id value=$mid>
+					<input type=hidden name=pass value=$mpass>
+					<input type=hidden name=rmode value=$in{'rmode'}>
+					<input type=hidden name=mode value=bankall>
+					<input type=submit value=全部存入 CLASS=MFC>
+				</form>
+				<form action="./town.cgi" method="post">
+					<input type=text size=5 name=hiki>萬Gold
+					<input type=hidden name=id value=$mid>
+					<input type=hidden name=pass value=$mpass>
+					<input type=hidden name=rmode value=$in{'rmode'}>
+					<input type=hidden name=mode value=bankh>
+					<input type=submit value=取出 CLASS=MFC>
+				</form>
+				<form action="./town.cgi" method="post">
+					<input type=hidden name=azuke value=$mbank>
+					<input type=hidden name=id value=$mid>
+					<input type=hidden name=pass value=$mpass>
+					<input type=hidden name=rmode value=$in{'rmode'}>
+					<input type=hidden name=mode value=bankhall>
+					<input type=submit value=全部取出 CLASS=MFC>
+				</form>
+				$BACKTOWNBUTTON
+			</td>
+			</tr>
+		</tbody>
+	</table>
 EOF
 
 	&footer;
--- town/bank2.pl
+++ town/bank2.pl
@@ -4,7 +4,6 @@ sub bank2{
 	if($in{'azuke'} eq "" && $in{'hiki'} eq ""){&error("請輸入金額。");}
 	if($in{'azuke'} =~ m/[^0-9]/ || $in{'hiki'} =~ m/[^0-9]/){&error("請輸入正確的金額。");}
 	if($in{'azuke'} <0 || $in{'hiki'} < 0){&error("請輸入正確的金額。");}
-	
 
 	if($in{'mode'} eq"banka"){
 		$com="存入";
@@ -12,14 +11,13 @@ sub bank2{
 		$mgold-=$gold;
 		if($mgold<0){&error("所持金不足。");}
 		$mbank+=$gold;
-                &ext_open;
-                &quest_open;
-                if($quest2_town_no eq $mpos && $quest2_limit_time>$date && $quest2_count<10){
-                        $quest2_count=0;
-                }
-                &quest_input;
-                &ext_input;
-
+		&ext_open;
+		&quest_open;
+		if($quest2_town_no eq $mpos && $quest2_limit_time>$date && $quest2_count<10){
+			$quest2_count=0;
+		}
+		&quest_input;
+		&ext_input;
 	}elsif($in{'mode'} eq"bankh"){
 		$com="取出";
 		$gold=$in{'hiki'}*10000;
@@ -34,12 +32,11 @@ sub bank2{
 		$mgold=0;
 		&ext_open;
 		&quest_open;
-	        if($quest2_town_no eq $mpos && $quest2_limit_time>$date && $quest2_count<10){
-	                $quest2_count=0;
-	        }
-	        &quest_input;
-	        &ext_input;
-
+		if($quest2_town_no eq $mpos && $quest2_limit_time>$date && $quest2_count<10){
+			$quest2_count=0;
+		}
+		&quest_input;
+		&ext_input;
 	}elsif($in{'mode'} eq"bankhall"){
 		$com="取出";
 		$gold=$in{'azuke'};
@@ -51,22 +48,22 @@ sub bank2{
 	&header;
 	
 	print <<"EOF";
-<table border="0" width="80%" bgcolor="#ffffff" height="150" align=center CLASS=FC>
-  <tbody>
-    <tr>
-      <td colspan="2" align="center" bgcolor="#993300"><font color="#ffffcc">銀行</font></td>
-    </tr>
-    <tr>
-      <td bgcolor="#ffffcc" width=20% align=center><img src="$IMG/etc/pub.jpg"></td>
-      <td bgcolor="#330000"><font color="#ffffcc">$gold Gold$com。</font></td>
-    </tr>
-    <tr>
-      <td colspan="2" align="right">
-$BACKTOWNBUTTON
-     </td>
-    </tr>
-  </tbody>
-</table>
+	<table border="0" width="80%" bgcolor="#ffffff" height="150" align=center CLASS=FC>
+		<tbody>
+			<tr>
+				<td colspan="2" align="center" bgcolor="#993300"><font color="#ffffcc">銀行</font></td>
+			</tr>
+			<tr>
+				<td bgcolor="#ffffcc" width=20% align=center><img src="$IMG/etc/pub.jpg"></td>
+				<td bgcolor="#330000"><font color="#ffffcc">$gold Gold$com。</font></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="right">
+					$BACKTOWNBUTTON
+				</td>
+			</tr>
+		</tbody>
+	</table>
 EOF
 	&chara_input;
 	&footer;
